e98a925d1974a1ee163ec28cfaca4513e4219877
Mattlite <mattlite@gmail.com>
2017-12-16 13:40:09 -0600

    refactor

--- Get-SpotlightImages.ps1
+++ Get-SpotlightImages.ps1
@@ -13,10 +13,10 @@ param
     [string] $BackgroundDir,
 
     [Parameter()]
-    [string] $BgTemp,
+    [string] $PhoneTemp,
 
     [Parameter()]
-    [string] $PhoneTemp,
+    [string] $Rejected,
 
     [Parameter()]
     [switch] $Sort
@@ -24,14 +24,16 @@ param
 
 # Copy over files from spotlight source if they are over 100kb in size
 Get-ChildItem $Source | Where-Object { $_.Length -gt 100000 } | Copy-Item -Destination $Destination
-$NewPics = Get-ChildItem $Destination
-$count = 0 
 
+$NewPics = Get-ChildItem $Destination
+$count = 10
 # Cycle through the pics and rename them with the date and a png extension
 foreach( $pic in $NewPics ){
     Rename-Item -Path $pic.FullName -NewName "$( Get-Date -Format M.d.yy )-$count.png"; $count++
 }
 
+# This sorts out the pics that are vertical for phone backgrounds
+#TODO: Check for duplicates on the phoneBgs
 if ( $Sort ) {
     # Import tools for getting picture meta data
     . $PSScriptRoot\Get-FileMetaDataReturnObject.ps1
@@ -40,6 +42,25 @@ if ( $Sort ) {
     $Data = Get-FileMetaData -folder $Destination
 
     # Move images to apporiate folder based on desktop for phone width
-    $Data | Where-Object { $_.Width -match "1920 pixels" } | Move-Item -Destination $BgTemp
-    $Data | Where-Object { $_.Width -match "1080 pixels" } | Move-Item -Destination $PhoneTemp
+    $Data | Where-Object { $_.Width -match "1080 pixels" } | Move-Item -Destination $PhoneTemp -Force
 }
+
+# Get the hash for existing pics and new pics, compare the two and delete the rejects
+$BadHash = Get-ChildItem $Rejected | Get-FileHash
+$NewHash = Get-ChildItem $Destination | Get-FileHash
+$OldHash = Get-ChildItem $BackgroundDir | Get-FileHash
+
+# First compare the new pictures with a list of pictures we don't want, then create a list of new good pictures.
+# Then compare the new good pictures with the existing pictures and copy over ones that don't already exsist.
+$NewGoodHash = Compare-Object $NewHash $BadHash -Property Hash -PassThru | Where-Object { $_.SideIndicator -eq '<=' }
+
+foreach( $hash in $NewGoodHash ){ 
+    Compare-Object $hash.Hash $OldHash -Property Hash -PassThru |
+        Where-Object { $_.SideIndicator -eq '<='  } |
+            ForEach-Object -Process {
+                Copy-Item $_.Path -Destination $BackgroundDir
+            }
+}
+
+# Clean up temp directory
+Get-ChildItem $Destination | Remove-Item -Force
